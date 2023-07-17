28c4e5b8f9a1ed04e6e22c0ef14c11a36b304a69
Matt McKnett <matt.mcknett@adadevelopersacademy.org>
2023-07-17 13:53:25 -0700

    Add sign-in

--- src/App.js
+++ src/App.js
@@ -3,14 +3,36 @@ import './App.css';
 import AddThing from './components/add-thing';
 import Things from './components/things';
 
+import app from './firebase';
+import { getAuth } from "firebase/auth";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+
 function App() {
+  const auth = getAuth(app);
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Firebase demo app</h1>
-        <AddThing />
-        <hr />
-        <Things />
+        {
+          !user && <button onClick={ () => signInWithGoogle() }>Sign In</button>
+        }
+        {
+          loading && "Signing in..."
+        }
+        {
+          error && "Sign in error: " + JSON.stringify(error)
+        }
+        {
+          user && (
+            <>
+              <AddThing />
+              <hr />
+              <Things />
+            </>
+          )
+        }
       </header>
     </div>
   );
