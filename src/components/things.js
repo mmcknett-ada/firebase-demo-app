f16c67577bec10e60451fd1a2b607db729b5236a
Matt McKnett <matt.mcknett@adadevelopersacademy.org>
2023-07-17 13:53:25 -0700

    Add loading the collection from firebase

--- src/components/things.js
+++ src/components/things.js
@@ -1,17 +1,38 @@
+import { getFirestore, collection } from 'firebase/firestore';
+import { useCollection } from 'react-firebase-hooks/firestore';
+
+import app from '../firebase.js';
+
 export default function Things(props) {
-  const FAKE_THING_LIST = [
-    { name: "Thing 1" },
-    { name: "Thing 2" }
-  ]
+  const [value, loading, error] = useCollection(
+    collection(getFirestore(app), 'things'),
+    {
+      snapshotListenOptions: { includeMetadataChanges: true },
+    }
+  );
 
-  const things = FAKE_THING_LIST;
+  // const FAKE_THING_LIST = [
+  //   { name: "Thing 1" },
+  //   { name: "Thing 2" }
+  // ]
 
+  const things = value && value.docs.map(doc => ({ name: doc.data().name }));
+
+  if (loading) {
+    return <article>Loading things...</article>
+  }
+
+  if (error) {
+    return <article>Failed to load things: {JSON.stringify(error)}</article>
+  }
+
+  console.log(value.docs, things);
   return (
     <article>
       <h2>All things:</h2>
-      <ul>
+      <ol>
         { things.map(thing => <li>{ thing.name }</li>) }
-      </ul>
+      </ol>
     </article>
   )
 }
